19e3b73bcae7a9ab29cacfde4c754fed37419334
joon <103298739+joonyee@users.noreply.github.com>
2025-02-24 02:21:41 +0900

    [level 0] Title: 배열 만들기 5, Time: 4.39 ms, Memory: 107 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < intStrs.length; i++) {
+            int tmp = Integer.parseInt(intStrs[i].substring(s, s + l));
+            if (tmp > k) {
+                list.add(tmp);
+            }
+        }
+        int[] answer = new int[list.size()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+}
