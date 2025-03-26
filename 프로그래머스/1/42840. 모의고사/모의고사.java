f183f938f39d50a4677b76d765573d814719a03f
joon <103298739+joonyee@users.noreply.github.com>
2025-03-26 15:34:18 +0900

    [level 1] Title: 모의고사, Time: 1.16 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -3,40 +3,40 @@ import java.util.*;
 
 class Solution {
     public ArrayList<Integer> solution(int[] answers) {
-        int[] answerCnt = {0, 0, 0};
+        //수포자의 정답 패턴 정의
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] arr3 = {3,3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        int[] a = {1, 2, 3, 4, 5};
-        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] answerCnt = new int[3];
+        ArrayList<Integer> result = new ArrayList<>();
         
-        for (int i = 0; i < answers.length; i++) {
-            if (a[i%a.length] == answers[i]) {
-                answerCnt[0] ++;
+        int count = 0;
+        //정답 배열과 비교하여 정답일 경우 점수 계산
+        for(int i=0; i<answers.length; i++){
+            if(answers[i] == arr1[i%5]){
+                answerCnt[0]++;
             }
-            if (b[i%b.length] == answers[i]) {
-                answerCnt[1] ++;
+        }
+        for(int i=0; i<answers.length; i++){
+            if(answers[i] == arr2[i%8]){
+                answerCnt[1]++;
             }
-            if (c[i%c.length] == answers[i]) {
-                answerCnt[2] ++;
+        }
+        for(int i=0; i<answers.length; i++){
+            if(answers[i] == arr3[i%10]){
+                answerCnt[2]++;
+            }
+        }
+        //가장 높은 점수를 받은 사람 리턴
+        int max = Math.max(answerCnt[0], Math.max(answerCnt[1], answerCnt[2]));
+        
+        for(int i=0; i<3; i++){
+            if(max == answerCnt[i]){
+                result.add(i+1);
             }
         }
         
-        int max = answerCnt[0];
-        if (max < answerCnt[1]) {
-            max = answerCnt[1];
-        }
-        if (max < answerCnt[2]) {
-            max = answerCnt[2];
-        }
-        
-        ArrayList<Integer> answerList = new ArrayList<>();
-        
-        for (int i = 0; i < 3; i++) {
-            if (max == answerCnt[i]) {
-                answerList.add(i+1);
-            }
-        }
-        
-        return answerList;
+        return result;
     }
 }
