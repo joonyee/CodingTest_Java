8210b45cf914788301ff7ed48df4ad939a3d2b9c
joon <103298739+joonyee@users.noreply.github.com>
2025-05-15 17:07:05 +0900

    [level 1] Title: 소수 만들기, Time: 1.98 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        int len = nums.length;
+
+        // 세 개의 수를 선택
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                for (int k = j + 1; k < len; k++) {
+                    // 세 수의 합이 소수인지 확인
+                    if (isPrime(nums[i] + nums[j] + nums[k])) {
+                        answer ++;
+                    }
+                }
+            }
+        }   
+        
+        return answer;
+    }
+    
+    // 2부터 n의 제곱근까지 나누어 소수인지 여부 확인
+    public boolean isPrime(int n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
