f63f80f8238a8f54af76a597cccf4b3be8bd3cdb
joon <103298739+joonyee@users.noreply.github.com>
2025-07-03 10:41:59 +0900

    [level 3] Title: 합승 택시 요금, Time: 57.12 ms, Memory: 68.3 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,44 +1,62 @@
 import java.util.*;
 
 class Solution {
+    //그래프 변수 선언
+    Map<Integer, List<int[]>> graph = new HashMap<>();
+    
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        List<int[]>[] graph = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
-        for (int[] f : fares) {
-            graph[f[0]].add(new int[]{f[1], f[2]});
-            graph[f[1]].add(new int[]{f[0], f[2]});
+        drawGraph(fares, n);
+        return lowCostTaxi(n, s, a, b);
+    }
+    
+    //drawGraph함수
+    public void drawGraph(int[][] fares, int n){
+        for(int i = 1; i<=n; i++){
+            graph.put(i, new ArrayList<>());
         }
-
-        int[] fromS = dijkstra(n, s, graph);
-        int[] fromA = dijkstra(n, a, graph);
-        int[] fromB = dijkstra(n, b, graph);
-
+        for(int[] fare : fares){
+            graph.get(fare[0]).add(new int[] {fare[1], fare[2]});
+            graph.get(fare[1]).add(new int[] {fare[0], fare[2]});
+        }
+    }
+    
+    public int lowCostTaxi(int n, int s, int a, int b){
+        int[] costS = dijkstra(n, s);
+        int[] costA = dijkstra(n, a);
+        int[] costB = dijkstra(n, b);
+        
         int min = Integer.MAX_VALUE;
-        for (int i = 1; i <= n; i++)
-            min = Math.min(min, fromS[i] + fromA[i] + fromB[i]);
-
+        
+        for(int i=1; i<=n; i++){
+            min = Math.min(min, costS[i] + costA[i] + costB[i]);
+        }
         return min;
     }
-
-    int[] dijkstra(int n, int start, List<int[]>[] graph) {
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[start] = 0;
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-        pq.add(new int[]{start, 0});
-
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            if (cur[1] > dist[cur[0]]) continue;
-
-            for (int[] next : graph[cur[0]]) {
-                if (dist[next[0]] > cur[1] + next[1]) {
-                    dist[next[0]] = cur[1] + next[1];
-                    pq.add(new int[]{next[0], dist[next[0]]});
-                }
+    
+    public int[] dijkstra(int n, int s){
+        
+        Queue<int[]> queue = new PriorityQueue<>((a1, a2) -> a1[1] - a2[1]);
+        queue.offer(new int[] {s, 0});
+        
+        int[] cost = new int[n+1];
+		    Arrays.fill(cost, Integer.MAX_VALUE);
+		    cost[s] = 0;
+        
+        while(!queue.isEmpty()){
+        int[] cur = queue.poll();
+        int curNode = cur[0];
+        int curCost = cur[1];
+        
+        for(int[] nodeInfo : graph.get(curNode)){
+            int nextNode = nodeInfo[0];
+            int nextCost = nodeInfo[1];
+            
+            if(curCost + nextCost < cost[nextNode]){
+                queue.offer(new int[] {nextNode, curCost + nextCost});
+                cost[nextNode] = curCost + nextCost;
             }
         }
-        return dist;
     }
+    return cost;
+  }   
 }
